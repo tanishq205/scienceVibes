412792eb7d35977860fed0ecf40b5b7d64a5bc05
tanishq <66425304+tanishq205@users.noreply.github.com>
2024-02-28 13:41:16 +0530

    upload

---
+++ script.js
@@ -0,0 +1,98 @@
+$(function() {
+    $(".gravity").hover(
+        function() {
+            $(this).attr("src", "assets/gravity.gif");
+        },
+        function() {
+            $(this).attr("src", "assets/gravity.png");
+        }                         
+    );                  
+});
+
+$(function() {
+    $(".force").hover(
+        function() {
+            $(this).attr("src", "assets/force.gif");
+        },
+        function() {
+            $(this).attr("src", "assets/force.png");
+        }                         
+    );                  
+});
+
+$(function() {
+    $(".pressure").hover(
+        function() {
+            $(this).attr("src", "assets/pressure.gif");
+        },
+        function() {
+            $(this).attr("src", "assets/pressure.png");
+        }                         
+    );                  
+});
+
+$(function() {
+    $(".friction").hover(
+        function() {
+            $(this).attr("src", "assets/friction.gif");
+        },
+        function() {
+            $(this).attr("src", "assets/friction.png");
+        }                         
+    );                  
+});
+
+$(function() {
+    $(".stress").hover(
+        function() {
+            $(this).attr("src", "assets/stress.gif");
+        },
+        function() {
+            $(this).attr("src", "assets/stress.png");
+        }                         
+    );                  
+});
+
+$(function() {
+    $(".surface_tension").hover(
+        function() {
+            $(this).attr("src", "assets/surface_tension.gif");
+        },
+        function() {
+            $(this).attr("src", "assets/surface_tension.png");
+        }                         
+    );                  
+});
+
+$(function() {
+    $(".electromagnet").hover(
+        function() {
+            $(this).attr("src", "assets/electromagnet.gif");
+        },
+        function() {
+            $(this).attr("src", "assets/electromagnet.png");
+        }                         
+    );                  
+});
+
+$(function() {
+    $(".areo").hover(
+        function() {
+            $(this).attr("src", "assets/areo.webp");
+        },
+        function() {
+            $(this).attr("src", "assets/areo.png");
+        }                         
+    );                  
+});
+
+$(function() {
+    $(".refraction").hover(
+        function() {
+            $(this).attr("src", "assets/refraction.gif");
+        },
+        function() {
+            $(this).attr("src", "assets/refraction.png");
+        }                         
+    );                  
+});
